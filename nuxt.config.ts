874b8a96c059d214e16ba080e9e30b663885a81f
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2024-08-21 03:39:31 -0500

    refactor: optimize config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,127 +1,79 @@
-import vuetify from "vite-plugin-vuetify";
-// PWA Config
-const title = "NuxtJS 3 + Vuetify 3 Customized";
-const shortTitle = "NuxtJS 3 + Vuetify 3 Customized";
-const description = "Template showing how to customize Vuetify SASS variables in a Nuxt3 webapp";
-const image = "https://v3customized.behonbaker.com/screenshot.png";
-const url = "https://v3customized.behonbaker.com/";
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+import vuetifySass from "@paro-paro/vite-plugin-vuetify-sass";
+import { SITE_TITLE, SITE_DESCRIPTION, SITE_URL, SITE_THEME_COLOR } from "./utils/seo";
 
 export default defineNuxtConfig({
-  // Enable takeover mode for better DX
-  typescript: { shim: false },
-
+  devtools: { enabled: true },
   // we have to transpile vuetify
   build: { transpile: ["vuetify"] },
-
-  // disable sourcemap so the terminal warnings go away
-  sourcemap: {
-    client: false,
-    server: false,
-  },
-
   // import styles
   css: ["vuetify/styles", "@/assets/main.scss"],
-
-  modules: [
-    "@kevinmarrec/nuxt-pwa",
-    async (options, nuxt) => {
-      nuxt.hooks.hook("vite:extendConfig", (config) =>
-        // @ts-ignore
-        config.plugins.push(
-          vuetify({
-            styles: { configFile: "assets/variables.scss" },
-          })
-        )
+  features: { inlineStyles: false },
+  hooks: {
+    "vite:extendConfig": (config) => {
+      config.plugins!.push(
+        vuetify({ autoImport: true }), // do not pass the 'styles' option
+        vuetifySass({
+          configFile: "assets/variables.scss",
+        })
       );
     },
-  ],
+  },
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+  modules: ["@vite-pwa/nuxt"],
 
-  // head config
   app: {
     head: {
-      title,
+      title: SITE_TITLE,
       link: [
         { rel: "preconnect", href: "https://rsms.me/" },
         { rel: "stylesheet", href: "https://rsms.me/inter/inter.css" },
         { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-        {
-          rel: "canonical",
-          href: url,
-        },
-      ],
-      meta: [
-        {
-          hid: "description",
-          name: "description",
-          content: description,
-        },
-        { property: "og:site_name", content: title },
-        { hid: "og:type", property: "og:type", content: "website" },
-        {
-          hid: "og:url",
-          property: "og:url",
-          content: url,
-        },
-        {
-          hid: "og:image:secure_url",
-          property: "og:image:secure_url",
-          content: image,
-        },
-        {
-          hid: "og:title",
-          property: "og:title",
-          content: title,
-        },
-        {
-          hid: "og:description",
-          property: "og:description",
-          content: description,
-        },
-        {
-          hid: "og:image",
-          property: "og:image",
-          content: image,
-        },
-        //Twitter
-        { name: "twitter:card", content: "summary_large_image" },
-        {
-          hid: "twitter:url",
-          name: "twitter:url",
-          content: url,
-        },
-        {
-          hid: "twitter:title",
-          name: "twitter:title",
-          content: title,
-        },
-        {
-          hid: "twitter:description",
-          name: "twitter:description",
-          content: description,
-        },
-        {
-          hid: "twitter:image",
-          name: "twitter:image",
-          content: image,
-        },
+        { rel: "canonical", href: SITE_URL },
       ],
     },
   },
-
   pwa: {
-    meta: {
-      name: shortTitle,
-      author: "Behon Baker",
-      theme_color: "#4f46e5",
-      description: description,
-    },
+    client: { installPrompt: false },
+    devOptions: { suppressWarnings: true },
+    includeAssets: ["favicon.ico", "robots.txt", "screenshot.png"],
     manifest: {
-      name: shortTitle,
-      short_name: shortTitle,
-      theme_color: "#4f46e5",
-      description: description,
+      background_color: "#FFFFFF",
+      description: SITE_DESCRIPTION,
+      icons: [
+        {
+          src: "icons/pwa-64x64.png",
+          sizes: "64x64",
+          type: "image/png",
+        },
+        {
+          src: "icons/pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "icons/pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "icons/maskable-icon-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable",
+        },
+      ],
+      lang: "en",
+      name: SITE_TITLE,
+      short_name: SITE_TITLE,
+      theme_color: SITE_THEME_COLOR,
     },
   },
-
   compatibilityDate: "2024-08-18",
 });
