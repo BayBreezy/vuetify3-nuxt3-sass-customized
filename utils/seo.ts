874bea055e59cf198f4a05bb842bcbbae8fba843
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2024-08-21 03:36:19 -0500

    chore(seo): add seo values in a separate file

---
+++ utils/seo.ts
@@ -0,0 +1,7 @@
+export const SITE_TITLE = "NuxtJS 3 + Vuetify 3 Customized";
+export const SITE_SHORT_TITLE = "NuxtJS 3 + Vuetify 3 Customized";
+export const SITE_DESCRIPTION =
+  "Template showing how to customize Vuetify SASS variables in a Nuxt3 webapp";
+export const SITE_IMAGE = "/screenshot.png";
+export const SITE_URL = "https://v3customized.behonbaker.com/";
+export const SITE_THEME_COLOR = "#4f46e5";
