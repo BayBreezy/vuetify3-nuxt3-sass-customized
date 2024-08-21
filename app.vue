77b2647c9204f88c005b0630d3a23840be58b136
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2024-08-21 03:31:26 -0500

    refactor: add seo data to app.vue file and add pwa component

--- app.vue
+++ app.vue
@@ -1,7 +1,28 @@
 <template>
-	<div>
-		<NuxtLayout>
-			<NuxtPage></NuxtPage>
-		</NuxtLayout>
-	</div>
+  <div>
+    <NuxtPwaAssets />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
+
+<script lang="ts" setup>
+useSeoMeta({
+  title: SITE_TITLE,
+  description: SITE_DESCRIPTION,
+  twitterTitle: SITE_TITLE,
+  twitterDescription: SITE_DESCRIPTION,
+  twitterImage: SITE_IMAGE,
+  twitterCard: "summary_large_image",
+  ogTitle: SITE_TITLE,
+  ogDescription: SITE_DESCRIPTION,
+  ogImage: SITE_IMAGE,
+  ogUrl: SITE_URL,
+  themeColor: SITE_THEME_COLOR,
+});
+useHead({
+  htmlAttrs: { lang: "en" },
+  link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+});
+</script>
